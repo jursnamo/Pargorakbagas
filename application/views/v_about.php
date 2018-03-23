adc3f965c40326133980fa7b9ac88a60a3bc1bc0
jursnamo <soalparna@gmail.com>
2018-03-24 02:19:17 +0700

    just commit

---
+++ application/views/v_about.php
@@ -0,0 +1,202 @@
+
+<!DOCTYPE html>
+<html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>About Us</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="M Fikri Setiadi" />
+	<link rel="shorcut icon" type="text/css" href="<?php echo base_url().'assets/images/favicon.png'?>">
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/animate.css'?>">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/icomoon.css'?>">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/bootstrap.css'?>">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/flexslider.css'?>">
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/style.css'?>">
+
+	<!-- Modernizr JS -->
+	<script src="<?php echo base_url().'theme/js/modernizr-2.6.2.min.js'?>"></script>
+
+	</head>
+	<body>
+
+
+	<div id="fh5co-page">
+	<header id="fh5co-header" role="banner">
+		<div class="container">
+			<div class="header-inner">
+				<h1><a href="<?php echo base_url().''?>">Jujur Sitanggang<span>.</span></a></h1>
+				<nav role="navigation">
+					<ul>
+						<li><a href="<?php echo base_url().''?>">Home</a></li>
+						<li class="active"><a href="<?php echo base_url().'about'?>">About</a></li>
+						<li><a href="<?php echo base_url().'portfolio'?>">Portfolio</a></li>
+						<li><a href="<?php echo base_url().'artikel'?>">Service</a></li>
+						<li><a href="<?php echo base_url().'gallery'?>">Gallery</a></li>
+						<li><a href="<?php echo base_url().'kontak'?>">Contact</a></li>
+						//<li class="cta"><a href="<?php echo base_url?php echo base_url().'portfolio'?>">Get started</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+
+
+	<aside id="fh5co-hero" clsas="js-fullheight">
+		<div class="flexslider js-fullheight">
+			<ul class="slides">
+		   	<li style="background-image: url(<?php echo base_url().'theme/images/slide_3.jpg'?>);">
+		   		<div class="overlay-gradient"></div>
+		   		<div class="container">
+		   			<div class="col-md-10 col-md-offset-1 text-center js-fullheight slider-text">
+		   				<div class="slider-text-inner">
+		   					<h2>Who We Are</h2>
+		   					<p class="fh5co-lead"> <a href="# target="_blank"></a></p>
+		   				</div>
+		   			</div>
+		   		</div>
+		   	</li>
+		  	</ul>
+	  	</div>
+	</aside>
+
+	<div class="fh5co-about animate-box">
+		<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+			<h2>About Us</h2>
+			<p>Starting in 2010, DNA is an establish institution held by expertise consultants with various background and proven track records
+
+Here we recognising the importance of all brand issues and inventing the potential of those as the brand solutions
+
+Therefore, it’s our obligation to ensure the implementation delivers the solution in its best fit
+.</p>
+		</div>
+		<div class="container">
+			<div class="col-md-6">
+				<figure>
+					<img src="<?php echo base_url().'theme/images/image_1.jpg'?>" alt="Free HTML5 Template" class="img-responsive">
+				</figure>
+			</div>
+			<div class="col-md-6">
+				<h3>Visi</h3>
+				<ul>
+					<li>As a trusted partner for reliable improvement and solutions.</li>
+				</ul>
+				<h3>Misi</h3>
+				<ul>
+					<li>Inspire, equipped and empower,Transforming agent, Innovative solution.</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div class="fh5co-team animate-box">
+		<div class="container">
+
+				<div class="row">
+					<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+						<h2>The Team</h2>
+						<p>Kami memiliki tim yang solid. one team, one spirit, and one goal.</p>
+					</div>
+					<div class="col-md-4 fh5co-staff">
+						<img src="<?php echo base_url().'theme/images/person3.jpg'?>" alt="Free HTML5 Bootstrap Template" class="img-responsive">
+						<h3>Amiamalia</h3>
+						<h4>Co-Founder, CEO</h4>
+						<p>telah berkiprah lebih dari 15 tahun di industri periklanan.telah berhasil mendampingi kesuksesan kampanye periklanan dari berbagai klien baik dari produk komersial unggulan maupun kegiatan sosial  dan politik di Indonesia.</p>
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-google"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-md-4 fh5co-staff">
+						<img src="<?php echo base_url().'theme/images/person2.jpg'?>" alt="Free HTML5 Bootstrap Template" class="img-responsive">
+						<h3>Yudi</h3>
+						<h4>Oiffice Manager, </h4>
+						<p>Yudi adalah seorang web designer lulusan perguruan tinggi di Universitas Indonesia.</p>
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-google"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-md-4 fh5co-staff">
+						<img src="<?php echo base_url().'theme/images/person4.jpg'?>" alt="Free HTML5 Bootstrap Template" class="img-responsive">
+						<h3>Selly</h3>
+						<h4>Accounting </h4>
+						<p>Selly adalah seorang Accounting yang sangat berpengalaman seperti SAP dan lain lain .</p>
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-google"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+				</div>
+		</div>
+	</div>
+
+
+	<div class="fh5co-services">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3 text-center fh5co-heading animate-box">
+					<h2>LAYANAN</h2>
+				</div>
+				<div class="col-md-4 text-center item-block">
+					<span class="icon"><img src="<?php echo base_url().'theme/images/30.svg'?>" class="img-responsive"></span>
+					<h3>ADVERTISING</h3>
+					<p>	Bussines Advertising antara lain Billboard dan LED display ( termasuk digital creative content).</p>
+					<p><a href="<?php echo base_url().'portfolio'?>" class="btn btn-primary btn-outline with-arrow">Learn more <i class="icon-arrow-right"></i></a></p>
+				</div>
+				<div class="col-md-4 text-center item-block">
+					<span class="icon"><img src="<?php echo base_url().'theme/images/18.svg'?>" class="img-responsive"></span>
+					<h3>CONSULTING & TRAINING</h3>
+					<p>	menawarkan solusi manajemen dan bisnis yang didukung oleh para konsultan berkualitas.</p>
+					<p><a href="<?php echo base_url().'portfolio'?>" class="btn btn-primary btn-outline with-arrow">Learn more <i class="icon-arrow-right"></i></a></p>
+				</div>
+				<div class="col-md-4 text-center item-block">
+					<span class="icon"><img src="<?php echo base_url().'theme/images/27.svg'?>" class="img-responsive"></span>
+					<h3>CITY BRANDING</h3>
+					<p>	Bangun identitas bisnis dan usaha anda di dunia Internet melalui Website.</p>
+					<p><a href="<?php echo base_url().'portfolio'?>" class="btn btn-primary btn-outline with-arrow">Learn more <i class="icon-arrow-right"></i></a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<?php $this->load->view('v_footer');?>
+	</div>
+
+
+	<!-- jQuery -->
+	<script src="<?php echo base_url().'theme/js/jquery.min.js'?>"></script>
+	<!-- jQuery Easing -->
+	<script src="<?php echo base_url().'theme/js/jquery.easing.1.3.js'?>"></script>
+	<!-- Bootstrap -->
+	<script src="<?php echo base_url().'theme/js/bootstrap.min.js'?>"></script>
+	<!-- Waypoints -->
+	<script src="<?php echo base_url().'theme/js/jquery.waypoints.min.js'?>"></script>
+	<!-- Flexslider -->
+	<script src="<?php echo base_url().'theme/js/jquery.flexslider-min.js'?>"></script>
+
+	<!-- MAIN JS -->
+	<script src="<?php echo base_url().'theme/js/main.js'?>"></script>
+
+	</body>
+</html>
---
+++ application/views/v_footer.php
@@ -0,0 +1,37 @@
+<footer id="fh5co-footer" role="contentinfo">
+
+		<div class="container">
+			<div class="col-md-3 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>About Us</h3>
+				<p>expertise consultants with various background and proven track records
+                   Here we recognising the importance of all brand issues and inventing.</p>
+			</div>
+			<div class="col-md-6 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>Contact</h3>
+				<ul class="float">
+					<li><a >Jln.Ir.H.Djuanda No.3A/154 Rt/Rw 01/09 Kel.Dago </a></li>
+					<li><a >Kec.Coblong Bandung </a></li>
+					<li><a >Phone : +685351364674</a></li>
+					<li><a >Email : soalparna@gmail.com</a></li>
+				</ul>
+			
+
+			</div>
+
+			<div class="col-md-2 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>Follow Us</h3>
+				<ul class="fh5co-social">
+					<li><a href="#"><i class="icon-twitter"></i></a></li>
+					<li><a href="#"><i class="icon-facebook"></i></a></li>
+					<li><a href="#"><i class="icon-google-plus"></i></a></li>
+					<li><a href="#"><i class="icon-instagram"></i></a></li>
+				</ul>
+			</div>
+
+
+			<div class="col-md-12 fh5co-copyright text-center">
+				<p>&copy; 2017 by <a href="#" target="_blank"Jujur Sitanggang </a>. All Rights Reserved.</p>
+			</div>
+
+		</div>
+	</footer>
